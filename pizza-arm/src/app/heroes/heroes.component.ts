57eabfd28c0097f816d08ec285019a7d765110ed
Lord <lord.pvi@gmail.com>
2018-11-23 23:55:03 +0300

    lesson 2-4

--- pizza-arm/src/app/heroes/heroes.component.ts
+++ pizza-arm/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroservice: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroservice.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
